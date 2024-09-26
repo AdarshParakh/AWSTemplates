9118201493dd2d6411a15ac044484cfb116828ec
AdarshParakh <adarshjain.ad@gmail.com>
2024-09-26 19:52:47 -0400

    Added EC2 CloudFormation template, deployment script, and README

--- ec2-templates/deploy_ec2.sh
+++ ec2-templates/deploy_ec2.sh
@@ -4,12 +4,16 @@ STACK_NAME="lilpaws-web-test-ec2"
 TEMPLATE_FILE="ec2_template.yaml"
 REGION="us-east-1"
 
-# Deploy CloudFormation Stack
+# Read the VPC ID as input
+read -p "Enter VPC ID: " VPC_ID
+
+# Deploy CloudFormation Stack with VPC ID parameter
 aws cloudformation deploy \
   --template-file $TEMPLATE_FILE \
   --stack-name $STACK_NAME \
   --capabilities CAPABILITY_NAMED_IAM \
-  --region $REGION
+  --region $REGION \
+  --parameter-overrides VPCID=$VPC_ID
 
 if [ $? -eq 0 ]; then
   echo "EC2 Stack deployed successfully!"
